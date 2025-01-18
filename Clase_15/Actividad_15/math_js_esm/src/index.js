9348e112da6c3bf9aa3fcb308b287ba9d58a0256
cristhianherrera01 <cristhianfelipe89@gmail.com>
2025-01-17 21:45:20 -0500

    se elimina la carpeta de la clase 15 para hacer el ajuste de node modules

--- Clase_15/Actividad_15/math_js_esm/src/index.js
+++
@@ -1,7 +0,0 @@
-import math from './math.mjs';
-
-console.log('Suma de 10 y 5:', math.sumar(10, 5));
-console.log('Resta de 20 y 8:', math.restar(20, 8));
-console.log('Multiplicación de 6 y 7:', math.multiplicar(6, 7));
-console.log('División de 15 y 3:', math.dividir(15, 3));
-console.log('División de 15 y 0:', math.dividir(15, 0));
--- Clase_15/Actividad_15/proyecto_node/src/funciones.js
+++
@@ -1,9 +0,0 @@
-export const obtenertitulo = () => 'Bootcamp Fullstack'
-
-export const obtenerdocente = (docente, mentor) => `El docente del bootcamp es ${docente} y el mentor es ${mentor}`
-
-/*
-module.exports = {
-    obtenertitulo,
-    obtenerdocente
-}*/
