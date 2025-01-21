ed5b41f13bbc904667f147710fcb03e2ea6fe688
cristhianherrera01 <cristhianfelipe89@gmail.com>
2025-01-21 17:04:58 -0500

    version commonJS de las actividad 1

---
+++ Clase_1/Actividad_1/src/server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const path= require('path')
+
+const iniciarServidor= (option) =>{
+    const {port,public_path='public'} = option
+    //console.log(port)
+    //console.log(public_path)
+
+    const app = express()
+
+    app.use(express.static(public_path))
+    app.get('/', (req, res)=>{
+        const indexPath = path.join(__dirname+`../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+    app.listen(port, () =>{
+        console.log(`Êjecuntado el servidor en el puerto: ${port}`)
+    })
+} 
+module.exports = {
+    iniciarServidor
+}
